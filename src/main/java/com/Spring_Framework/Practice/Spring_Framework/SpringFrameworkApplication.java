6292a4232385608538c7744de132568cc28fd1e3
Shreyash2246 <spatil2246b@gmail.com>
2025-12-07 00:01:10 +0530

    added autowired and component annotation

--- src/main/java/com/Spring_Framework/Practice/Spring_Framework/SpringFrameworkApplication.java
+++ src/main/java/com/Spring_Framework/Practice/Spring_Framework/SpringFrameworkApplication.java
@@ -1,5 +1,6 @@
 package com.Spring_Framework.Practice.Spring_Framework;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,15 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringFrameworkApplication implements CommandLineRunner{
 
+	@Autowired
 	demo d;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringFrameworkApplication.class, args);
 	
 	}
 
+	
 	@Override
 	public void run(String... args) throws Exception {
-		
+		// generates runtime error
 		d.display();
 	}
 
