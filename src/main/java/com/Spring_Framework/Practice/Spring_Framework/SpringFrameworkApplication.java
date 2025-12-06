0a4adefc3cab4f6291667160820c55fa09c847ac
Shreyash2246 <spatil2246b@gmail.com>
2025-12-06 14:55:15 +0530

    Initial commit: Spring Framework setup

---
+++ src/main/java/com/Spring_Framework/Practice/Spring_Framework/SpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.Spring_Framework.Practice.Spring_Framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Spring_Framework/Practice/Spring_Framework/SpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring_Framework.Practice.Spring_Framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
